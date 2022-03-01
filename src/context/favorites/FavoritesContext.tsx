851625e4fc37a503e9878b80ae6b0b165b23b23c
ihist <ihist@suntek.io>
2022-03-01 21:39:48 +0300

    implementing storage

--- src/context/favorites/FavoritesContext.tsx
+++ src/context/favorites/FavoritesContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 import { IGiphy } from '../../models/giphy.model';
 
@@ -7,14 +7,29 @@ interface IFavoritesContext {
   setFavorites?: React.Dispatch<React.SetStateAction<IGiphy[]>>;
 }
 
+function getStorageValue(key: string, defaultValue: any) {
+  // getting stored value
+  if (typeof window !== 'undefined') {
+    const saved = localStorage.getItem(key);
+    const initial = saved !== null ? JSON.parse(saved) : defaultValue;
+    return initial;
+  }
+}
+
+const STORAGE_KEY = 'favorites';
+
 const defaultState = {
-  favorites: [],
+  favorites: getStorageValue(STORAGE_KEY, []),
 };
 
 export const FavoritesContext = createContext<IFavoritesContext>(defaultState);
 
 export const FavoritesProvider: React.FC = ({ children }) => {
   const [favorites, setFavorites] = useState<IGiphy[]>(defaultState.favorites);
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(favorites));
+  }, [favorites]);
 
   return (
     <FavoritesContext.Provider value={{ favorites, setFavorites }}>
